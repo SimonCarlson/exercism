9c9ab0821b9873aed4a5cb4210ff9a8a937d823b
Simon Carlson <cimon.carlson@gmail.com>
2018-08-13 17:28:51 +0200

    Start of encode

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,6 +1,16 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    let cipher: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().rev().collect();
+    let mut crypto = String::new();
+    for c in plain.replace(" ", "").replace(",", "").replace(".", "").to_ascii_lowercase().chars() {
+        if c.is_digit(10) {
+            crypto.push(c)
+        } else {
+            crypto.push(cipher[c as usize - 97])
+        }
+    };
+
+    crypto
 }
 
 /// "Decipher" with the Atbash cipher.
