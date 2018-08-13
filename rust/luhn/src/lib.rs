da5bdf02be013fa98635dbda33cab79199f6f4ed
Simon Carlson <cimon.carlson@gmail.com>
2018-08-13 17:00:39 +0200

    Finished luhn implementation

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -3,7 +3,7 @@ pub fn is_valid(input_code: &str) -> bool {
     let code = String::from(input_code);
     let mut stripped_code = String::new();
 
-    if code.len() < 2 {
+    if code.trim().len() < 2 {
         return false
     }
 
@@ -23,12 +23,37 @@ pub fn is_valid(input_code: &str) -> bool {
 
     // Double every second number from the right
     // If the doubling exceeds 9, subtract 9
+    // If stripped_code.len is even, start from zeroth digit. If odd, start from first
+    // Iterate over stripped_code with step 2
+    let skip = stripped_code.len() % 2;
+    let mut double_sum = 0;
+    for c in stripped_code.chars().skip(skip).step_by(2) {
+        let mut digit;
+        match c.to_digit(10) {
+            Some(i) => digit = i * 2,
+            _ => return false,
+        };
 
-    // Put the doubled numbers back in order with the rest of the numbers
+        if digit > 9 {
+            digit -= 9
+        };
 
-    // Sum the digits
+        double_sum += digit;
+        digit = 0;
 
-    // If divisible by 10, pass
+    };
+
+    // Sum up the numbers that werent doubled
+    for c in stripped_code.chars().skip(1 - skip).step_by(2) {
+        match c.to_digit(10) {
+            Some(i) => double_sum += i,
+            _ => return false,
+        }
+    };
+
+    if double_sum % 10 == 0 {
+        return true
+    };
 
     false
 }
