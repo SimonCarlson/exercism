a7263c1fb77aae6b05194b5ed0c0dbd0902601d3
Simon Carlson <cimon.carlson@gmail.com>
2018-08-16 09:13:30 +0200

    PartialEq trait over T, implements sublist equality

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,7 +1,22 @@
-enum Comparison {
+#[derive(PartialEq, Debug)]
+pub enum Comparison {
     Equal,
     Sublist,
     Superlist,
     Unequal
-};
+}
 
+
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
+    if a.len() == b.len() {
+        for (i, j) in a.iter().zip(b.iter()) {
+            if i != j {
+                break
+            }
+        }
+
+        return Comparison::Equal
+    };
+
+    Comparison::Unequal
+}
