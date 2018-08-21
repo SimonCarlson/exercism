2893d93180d41bf0a3697708276764bc65024dff
Simon Carlson <cimon.carlson@gmail.com>
2018-08-21 09:59:04 +0200

    Sort the list of hand type

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,15 +1,20 @@
 use std::collections::HashMap;
+use std::cmp::Ordering;
 
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
-    let mut hm = HashMap::new();
+    let mut hm: HashMap<&str, Hand> = HashMap::new();
     for hand in hands.iter() {
         let hand_type = find_hand_type(hand);
         hm.insert(hand, hand_type);
     };
+
+    let mut winners: Vec<_> = hm.iter().collect();
+    winners.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Greater));
+
 
     return None
 }
