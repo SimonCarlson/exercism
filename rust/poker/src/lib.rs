50af6d51cc5a38a650830814bc43a25634f773ef
Simon Carlson <cimon.carlson@gmail.com>
2018-08-21 09:51:12 +0200

    Rough plan, compiles

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,13 +1,25 @@
+use std::collections::HashMap;
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
-    unimplemented!("Out of {:?}, which hand wins?", hands)
+    let mut hm = HashMap::new();
+    for hand in hands.iter() {
+        let hand_type = find_hand_type(hand);
+        hm.insert(hand, hand_type);
+    };
+
+    return None
 }
 
-#[derive(PartialOrd)]
-enum Hand {
+pub fn find_hand_type(hand: &&str) -> Hand {
+    Hand::HighCard
+}
+
+#[derive(PartialOrd, PartialEq)]
+pub enum Hand {
     StraightFlush,
     FourOfAKind,
     FullHouse,
