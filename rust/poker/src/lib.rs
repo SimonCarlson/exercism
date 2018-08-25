c811fe33e48dc36b9ee1e610510e03f5b8cc1295
Simon Carlson <cimon.carlson@gmail.com>
2018-08-25 12:37:59 +0200

    Hand, Card, Suite, and Rank abstractions

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -3,13 +3,63 @@
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
+
     None
 }
 
 struct Hand {
-    HandType: HandType,
-    Score: u32,
+    cards: Vec<Card>,
+    score: u32,
 }
+
+struct Card {
+    rank: Rank,
+    suite: Suite,
+}
+
+impl Card {
+    fn from(card_string: &str) -> Card {
+        let (head, tail) = card_string.split_at(card_string.len() - 1);
+        Card {
+            rank: Rank::from(head),
+            suite: Suite::from(tail)
+        }
+    }
+}
+
+enum Suite {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES,
+}
+
+impl Suite {
+    fn from(suite: &str) -> Suite {
+        match suite {
+            "C" => Suite::CLUBS,
+            "D" => Suite::DIAMONDS,
+            "H" => Suite::HEARTS,
+            "S" => Suite::SPADES,
+            _ => panic!("Invalid suite")
+        }
+    }
+}
+
+struct Rank(u32);
+
+impl Rank {
+    fn from(rank: &str) -> Rank {
+        match rank {
+            "A" => Rank(14),
+            "K" => Rank(13),
+            "Q" => Rank(12),
+            "J" => Rank(11),
+            v => Rank(v.parse().unwrap())
+        }
+    }
+}
+
 
 #[derive(PartialOrd, PartialEq)]
 enum HandType {
