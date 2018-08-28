230c858e281e1cef912942da4986ceee504295bd
Simon Carlson <cimon.carlson@gmail.com>
2018-08-28 09:46:02 +0200

    Initial thought, build and sort vectors

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,23 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut is_anagram = true;
+    let mut return_values: HashSet<&'a str> = HashSet::new();
+    let mut word_vec: Vec<_> = word.to_ascii_lowercase().chars().collect();
+    word_vec.sort_by(|a, b| a.cmp(b));
+    println!("{:?}", word_vec);
+
+    for anagram in possible_anagrams {
+        let mut anagram_vec: Vec<_> = anagram.to_ascii_lowercase().chars().collect();
+        anagram_vec.sort_by(|a, b| a.cmp(b));
+        println!("{:?}", anagram_vec);
+        
+
+        if word_vec == anagram_vec {
+            return_values.insert(anagram);
+        }
+        is_anagram = true;
+    }
+
+    return_values
 }
