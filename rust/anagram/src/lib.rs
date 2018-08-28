dc7534a9918709c52b99193e6b8d500c9ac1b6d1
Simon Carlson <cimon.carlson@gmail.com>
2018-08-28 09:48:33 +0200

    Apparently only first word should be case insensitive (?)

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -3,12 +3,14 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut is_anagram = true;
     let mut return_values: HashSet<&'a str> = HashSet::new();
-    let mut word_vec: Vec<_> = word.to_ascii_lowercase().chars().collect();
+    let mut word_vec: Vec<_> = word.chars().collect();
+    word_vec[0].make_ascii_lowercase();
     word_vec.sort_by(|a, b| a.cmp(b));
     println!("{:?}", word_vec);
 
     for anagram in possible_anagrams {
-        let mut anagram_vec: Vec<_> = anagram.to_ascii_lowercase().chars().collect();
+        let mut anagram_vec: Vec<_> = anagram.chars().collect();
+        anagram_vec[0].make_ascii_lowercase();
         anagram_vec.sort_by(|a, b| a.cmp(b));
         println!("{:?}", anagram_vec);
         
